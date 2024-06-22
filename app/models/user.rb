266009b183971534c741a22ba92c6c3b53c5252a
rettofe <ferettosan@gmail.com>
2024-06-22 09:18:52 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         has_many :prototypes
+         has_many :comments
+
+  validates :position, presence: true
+  validates :occupation, presence: true
+  validates :profile, presence: true
+  validates :name, presence: true
+  validates :encrypted_password, presence: true
+  validates :email, presence: true
+
+end
